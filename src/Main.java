b77a51a8b0b3bc24d170ff0ed14a84d80b5cab1c
Wild-Bill <spamy4@mail.ru>
2023-03-11 01:26:45 +0300

    add extact-metod

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,54 +17,64 @@ public class Main {
 
             switch (productCount) {
                 case 1:
-                    System.out.println("Какую покупку хотите добавить?");
-                    String product = scanner.next();
-                    list.add(product);
-                    int sum = 0;
-                    for (String i : list) {
-                        sum++;
-                    }
-                    System.out.println("Итого в Списке покупок:" + sum + "");
+                    addProduct(scanner, list);
                     break;
 
                 case 2:
-                    System.out.println("Список покупок:");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + "." + list.get(i));
-                    }
+                    shoppingList(list);
                     break;
                 case 3:
-                    System.out.println("Список покупок:");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + "." + list.get(i));
-                    }
-                    System.out.println("Какую хотите удалить? ВВедите номер или название:");
-                    String numberName = scanner.next();
-                    try {
-                        int index = Integer.parseInt(numberName) - 1;
-                        list.remove(index);
-                    } catch (Exception e) {
-                        list.remove(numberName);
-                    }
+                    shoppingList(list);
+                    deleteProduct(scanner, list);
                     break;
                 case 4:
-                    System.out.println("Введите текст для поиска::");
-                    String text = scanner.next();
-                    String queryLower;
-                    String itemLower;
-                    queryLower = text.toLowerCase();
-                    for (int i = 0; i < list.size(); i++) {
-                        itemLower = list.get(i).toLowerCase();
-                        if (itemLower.contains(queryLower)) {
-                            System.out.println((i + 1) + "." + list.get(i));
-                        }
-                    }
+                    searchProduct(scanner, list);
                     break;
                 default:
                     System.out.println("Ввели не правильную команду");
             }
         }
-
     }
 
+    private static void searchProduct(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Введите текст для поиска:");
+        String text = scanner.next();
+        String queryLower;
+        queryLower = text.toLowerCase();
+        String itemLower;
+        for (int i = 0; i < list.size(); i++) {
+            itemLower = list.get(i).toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                System.out.println((i + 1) + "." + list.get(i));
+            }
+        }
+    }
+
+    private static void addProduct(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Какую покупку хотите добавить?");
+        list.add(scanner.next());
+        int sum = 0;
+        for (String i : list) {
+            sum++;
+        }
+        System.out.println("Итого в Списке покупок:" + sum + "");
+    }
+
+    private static void deleteProduct(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Какую хотите удалить? ВВедите номер или название:");
+        String numberName = scanner.next();
+        try {
+            int index = Integer.parseInt(numberName) - 1;
+            list.remove(index);
+        } catch (Exception e) {
+            list.remove(numberName);
+        }
+    }
+
+    private static void shoppingList(List<String> list) {
+        System.out.println("Список покупок:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + "." + list.get(i));
+        }
+    }
 }
